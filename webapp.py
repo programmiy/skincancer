4beee05367ea8a3dcd4859f8654fec4081234e6b
programmiy <yyh4814@kakao.com>
2023-05-06 23:42:21 +0900

    검사하기 처음

--- webapp.py
+++ webapp.py
@@ -175,59 +175,82 @@ def testing():
     import streamlit as st
     import pandas as pd
     import altair as alt
+    import time
+    import tensorflow as tf
+    from tcp_latency import measure_latency
+    import numpy as np
+    import pandas as pd 
 
+
+    # Measure Latency
+    #18.155.181.8
+    #35.201.127.49:443
+    #192.168.18.6:8501
+
+    # Load TensorFlow Lite model
+    interpreter = tf.lite.Interpreter(model_path="mobilenet_v1_1.0_224_quant.tflite")
+    interpreter.allocate_tensors()
+    interpreter = tf.lite.Interpreter(model_path="InceptionResNetV2Skripsi.tflite")
+    interpreter.allocate_tensors()
+
+    # Get input and output tensors
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    # Define a function to resize the input image
+    def resize_image(image):
+        # Resize the image to 150x150 pixels
+        resized_image = tf.image.resize(image, [150, 150])
+        return resized_image.numpy()
+
+    # Define a function to run inference on the TensorFlow Lite model
+    def classify_image(image):
+        # Pre-process the input image
+        resized_image = resize_image(image)
+        input_data = np.expand_dims(resized_image, axis=0).astype(np.float32)
+        interpreter.set_tensor(input_details[0]['index'], input_data)
+
+        # Run inference
+        with st.spinner('Classifying...'):
+            start_time = time.time()
+            interpreter.invoke()
+            end_time = time.time()
+
+        # Calculate the classification duration
+        classifying_duration = end_time - start_time
+
+        # Get the output probabilities
+        output_data = interpreter.get_tensor(output_details[0]['index'])
+
+        return output_data[0], classifying_duration
+
+    # Define the labels for the 7 classes
+    labels = ['akiec', 'bcc', 'bkl', 'df', 'mel', 'nv', 'vasc']
+
+    from PIL import Image
     from urllib.error import URLError
 
-    st.markdown(f"# {list(page_names_to_funcs.keys())[3]}")
-    st.write(
-        """
-        This demo shows how to use `st.write` to visualize Pandas DataFrames.
+       # Get the input image from the user
+    image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
-(Data courtesy of the [UN Data Explorer](http://data.un.org/Explorer.aspx).)
-"""
-    )
+    # Show the input image
+    if image is not None:
+        image = np.array(Image.open(image).convert("RGB"))
+        st.image(image, width=150)
 
-    @st.cache_data
-    def get_UN_data():
-        AWS_BUCKET_URL = "http://streamlit-demo-data.s3-us-west-2.amazonaws.com"
-        df = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")
-        return df.set_index("Region")
+        # Run inference on the input image
+        probs, classifying_duration = classify_image(image)
 
-    try:
-        df = get_UN_data()
-        countries = st.multiselect(
-            "Choose countries", list(df.index), ["China", "United States of America"]
-        )
-        if not countries:
-            st.error("Please select at least one country.")
-        else:
-            data = df.loc[countries]
-            data /= 1000000.0
-            st.write("### Gross Agricultural Production ($B)", data.sort_index())
+        # Display the classification duration
+        st.write(f"Classification duration: {classifying_duration:.4f} seconds")
 
-            data = data.T.reset_index()
-            data = pd.melt(data, id_vars=["index"]).rename(
-                columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
-            )
-            chart = (
-                alt.Chart(data)
-                .mark_area(opacity=0.3)
-                .encode(
-                    x="year:T",
-                    y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
-                    color="Region:N",
-                )
-            )
-            st.altair_chart(chart, use_container_width=True)
-    except URLError as e:
-        st.error(
-            """
-            **This demo requires internet access.**
-
-            Connection error: %s
-        """
-            % e.reason
-        )
+        # Display the top 3 predictions
+        top_3_indices = np.argsort(probs)[::-1][:3]
+        st.write("Top 3 predictions:")
+        for i in range(3):
+            st.write("%d. %s (%.2f%%)" % (i + 1, labels[top_3_indices[i]], probs[top_3_indices[i]] * 100))
+            
+        
 
 page_names_to_funcs = {
     "홈": intro,
