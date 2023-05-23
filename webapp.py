ebb91f3e44730b8931db8411b8847a277242237c
programmiy <yyh4814@kakao.com>
2023-05-23 16:46:10 +0900

    결과출력중간저장

--- webapp.py
+++ webapp.py
@@ -1,3 +1,5 @@
+import PIL
+from cv2 import normalize
 import streamlit as st
 import os
 import datetime
@@ -12,7 +14,10 @@ def classification(compare_img):
     import glob
     import io
     from PIL import Image
-
+    from reportlab.pdfgen import canvas
+    from reportlab.pdfgen import canvas
+    from reportlab.pdfbase import pdfmetrics
+    from reportlab.pdfbase.ttfonts import TTFont
     img_list = glob.glob('cancer/*.jpg')
     compare_img = np.array(compare_img)
     compare_img = cv2.cvtColor(compare_img, cv2.COLOR_RGB2BGR) 
@@ -42,6 +47,25 @@ def classification(compare_img):
     # get top 5 matches
     top_matches = matches_data[:5]
 
+
+
+   # Create a PDF canvas
+    pdf = canvas.Canvas('matched_images.pdf')
+
+    pdfmetrics.registerFont(TTFont('a조약돌', r'C:\windows\fonts\a조약돌.TTF'))
+
+    # Set the page dimensions
+    page_width = 595.276  # A4 page width in points
+    page_height = 841.890  # A4 page height in points
+
+    # Calculate the image and caption positions
+    image_width = 400
+    image_height = 300
+    image_x = (page_width - image_width) / 2
+    image_y = (page_height - image_height) / 2
+    caption_y = image_y - 20
+
+
     # Display the matched images
     for i, (img_path, similarity) in enumerate(top_matches):
         base_img = cv2.imread(img_path)
@@ -59,7 +83,41 @@ def classification(compare_img):
         img_bytes = cv2.imencode('.png', img3)[1].tobytes()
         st.image(Image.open(io.BytesIO(img_bytes)), caption=f'비슷한 이미지 #{i+1} (유사도: {similarity:.2f})') # 비활성화
 
-    
+
+       
+        
+         # Convert OpenCV image to PIL image
+        pil_image = Image.fromarray(cv2.cvtColor(img3, cv2.COLOR_BGR2RGB))
+
+        # Save PIL image as a temporary file
+        temp_filename = f'temp_image_{i}.png'
+        pil_image.save(temp_filename)
+
+        # Add the image to the PDF canvas
+        pdf.drawInlineImage(temp_filename, image_x, image_y, width=image_width, height=image_height)
+
+        # Add the caption below the image
+        caption = f'비슷한 이미지 #{i+1} (유사도: {similarity:.2f})'
+        pdf.setFont('a조약돌', 12)  # Use the registered custom font
+        caption_width = pdf.stringWidth(caption, 'a조약돌', 12)
+        caption_x = (page_width - caption_width) / 2
+        pdf.drawString(caption_x, caption_y, caption)
+
+        # Delete the temporary file
+        os.remove(temp_filename)
+
+        # Add a new page if there are more images
+        if i < len(matches_data) - 1:
+            pdf.showPage()
+
+
+    st.text("---------------------------------------------")
+    # Save the PDF file
+    pdf.save()
+
+
+
+
 
 
 def get_current_date():
@@ -93,7 +151,7 @@ def intro():
     """)
     st.sidebar.success("원하는 정보를 확인하세요")
     code = '''powered by Streamlit'''
-    st.code(code, language='python')
+    st.code(code, language='java')
     st.subheader("⚠️ 의학적 지식을 대체하는 것이 아닙니다 ")
     st.subheader("⚠️ 잠재적인 피부 문제를 쉽게 파악하는 도움을 받을 수 있도록 돕는 도구라는 점에 유의해야 합니다")
     st.markdown(
@@ -121,10 +179,10 @@ def intro():
     user = get_visitor_count(date)
     now = datetime.datetime.now()
     
-    def get_delta(now):
+    def get_delta(now): #파라미터 없애기, 혹은  get_current_date 수정
         now = datetime.datetime.now()
         delta = now - datetime.timedelta(days= 1)
-        delta = delta.strftime("%Y-%m-%d")
+        delta = delta.strftime("%Y-%m-%d") # get_current_date랑 겹침
         
         return delta
         
@@ -182,26 +240,57 @@ def testing():
 
 
     # Load TensorFlow Lite model
-    interpreter = tf.lite.Interpreter(model_path="mobilenet_v1_1.0_224_quant.tflite")
+    interpreter = tf.lite.Interpreter(model_path="models/mobilenet_v1_1.0_224_quant.tflite")
     interpreter.allocate_tensors()
-    interpreter = tf.lite.Interpreter(model_path="InceptionResNetV2Skripsi.tflite")
+    interpreter = tf.lite.Interpreter(model_path="models/InceptionResNetV2Skripsi.tflite")
     interpreter.allocate_tensors()
 
     # Get input and output tensors
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
 
+
+    # 이미지 업스케일링 
+
+
+   # default upsc
+
     # Define a function to resize the input image
-    def resize_image(image):
-        # Resize the image to 150x150 pixels
-        resized_image = tf.image.resize(image, [150, 150])
+    # def resize_image(image):
+    #     # Resize the image to 150x150 pixels
+    #     resized_image = tf.image.resize(image, [150, 150])
         
-        return resized_image.numpy()
+    #     return resized_image.numpy()
+
+
+    # replace upsc
+
+
+    def resize_image(image):
+        from PIL import Image
+        # Get the original image shape
+        original_shape = tf.shape(image)
+        
+        # Calculate the padding amounts
+        height_pad = (original_shape[0] - 150) // 2
+        width_pad = (original_shape[1] - 150) // 2
+        
+        # Pad the image
+        padded_image = tf.pad(image, [[height_pad, height_pad], [width_pad, width_pad], [0, 0]])
+        
+        # Resize the padded image to 150x150 pixels
+        resized_image = tf.image.resize(padded_image, [150, 150])
+
+        
+        return resized_image
+
 
     # Define a function to run inference on the TensorFlow Lite model
     def classify_image(image):
         # Pre-process the input image
         resized_image = resize_image(image)
+        
+
         input_data = np.expand_dims(resized_image, axis=0).astype(np.float32)
         interpreter.set_tensor(input_details[0]['index'], input_data)
         
@@ -238,8 +327,10 @@ def testing():
     if upload:
         image = st.file_uploader(label="파일 업로드하기", type=["jpg", "jpeg", "png"])
         if image is not None:
+            # image_upscailing(image)
             image = np.array(Image.open(image).convert("RGB"))
             preresize = image
+            st.code("작업에 사용된 이미지", language= "java")
             st.image(image, width=150)
 
             # Run inference on the input image
@@ -263,9 +354,10 @@ def testing():
     elif take:
         image = st.camera_input(label="사진 찍기", help="웹캠 지원")
         if image is not None:
+            
             image = np.array(Image.open(image).convert("RGB"))
             preresize = image
-            st.image(image, width=150) # 비활성화
+            
 
             # Run inference on the input image
             probs, classifying_duration = classify_image(image)
@@ -288,7 +380,7 @@ def testing():
 
         
 
-    
+# 페이지 구성 
 
 page_names_to_funcs = {
     "홈": intro,
